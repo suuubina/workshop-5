f07cfe5c05ddfae2d76c3358a124f7536563f0af
subina.suthanthiranathan@edu.devinci.fr <subina.Suthanthiranathan@edu.devinci.fr>
2024-03-26 00:25:43 +0100

    Test

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -1,64 +1,160 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { BASE_NODE_PORT } from "../config";
-import { Value } from "../types";
+import { NodeState, Value } from "../types";
+import { delay } from "../utils";
 
 export async function node(
-  nodeId: number, // the ID of the node
-  N: number, // total number of nodes in the network
-  F: number, // number of faulty nodes in the network
-  initialValue: Value, // initial value of the node
-  isFaulty: boolean, // true if the node is faulty, false otherwise
-  nodesAreReady: () => boolean, // used to know if all nodes are ready to receive requests
-  setNodeIsReady: (index: number) => void // this should be called when the node is started and ready to receive requests
+    nodeId: number,
+    N: number,
+    F: number,
+    initialValue: Value,
+    isFaulty: boolean,
+    nodesAreReady: () => boolean,
+    setNodeIsReady: (index: number) => void
 ) {
   const node = express();
   node.use(express.json());
   node.use(bodyParser.json());
 
-  // Object to store the state of the node
-  let state: Value = initialValue;
+  let currentNodeState: NodeState = {
+    killed: false,
+    x: null,
+    decided: null,
+    k: null,
+  }
+  let proposals: Map<number, Value[]> = new Map();
+  let votes: Map<number, Value[]> = new Map();
 
-  // Route to retrieve the current status of the node
-  node.get("/status", (req, res) => {
-    res.json({
-      nodeId,
-      state
-    });
-  });
 
-  // Route to receive messages from other nodes
-  node.post("/message", (req, res) => {
-    // Example of handling incoming messages
-    const { senderId, message } = req.body;
-    console.log(`Received message from Node ${senderId}: ${message}`);
-
-// TODO implement this
-  // this route allows retrieving the current status of the node
   node.get("/status", (req, res) => {
     if (isFaulty) {
-      res.status(500).send("faulty");
+      return res.status(500).send("faulty");
     } else {
-      res.status(200).send("live");
+      return res.status(200).send("live");
     }
   });
 
-    
-  // Route to get the current state of a node
+
   node.get("/getState", (req, res) => {
-    res.json({
-      nodeId,
-      state
+    res.status(200).send({
+      killed: currentNodeState.killed,
+      x: currentNodeState.x,
+      decided: currentNodeState.decided,
+      k: currentNodeState.k,
     });
   });
 
-  // Start the server
-  const server = node.listen(BASE_NODE_PORT + nodeId, async () => {
-    console.log(
-      `Node ${nodeId} is listening on port ${BASE_NODE_PORT + nodeId}`
-    );
+  node.get("/stop", (req, res) => {
+    currentNodeState.killed = true;
+    return res.status(200).send("killed");
+  });
 
-    // Mark the node as ready
+
+  node.post("/message", async (req, res) => {
+    let { k, x, messageType } = req.body;
+    if (!isFaulty && !currentNodeState.killed) {
+      if (messageType == "propose") {
+        if (!proposals.has(k)) {
+          proposals.set(k, []);
+        }
+        proposals.get(k)!.push(x); 
+        let proposal = proposals.get(k)!;
+
+        if (proposal.length >= (N - F)) {
+          let count0 = proposal.filter((el) => el == 0).length;
+          let count1 = proposal.filter((el) => el == 1).length;
+          if (count0 > (N / 2)) {
+            x = 0;
+          } else if (count1 > (N / 2)) {
+            x = 1;
+          } else {
+            x = "?";
+          }
+          for (let i = 0; i < N; i++) {
+            fetch(`http://localhost:${BASE_NODE_PORT + i}/message`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ k: k, x: x, messageType: "vote" }),
+            });
+          }
+        }
+      }
+      else if (messageType == "vote") {
+        if (!votes.has(k)) {
+          votes.set(k, []);
+        }
+        votes.get(k)!.push(x)
+        let vote = votes.get(k)!;
+          if (vote.length >= (N - F)) {
+            console.log("vote", vote,"node :",nodeId,"k :",k)
+            let count0 = vote.filter((el) => el == 0).length;
+            let count1 = vote.filter((el) => el == 1).length;
+
+            if (count0 >= F + 1) {
+              currentNodeState.x = 0;
+              currentNodeState.decided = true;
+            } else if (count1 >= F + 1) {
+              currentNodeState.x = 1;
+              currentNodeState.decided = true;
+            } else {
+              if (count0 + count1 > 0 && count0 > count1) {
+                currentNodeState.x = 0;
+              } else if (count0 + count1 > 0 && count0 < count1) {
+                currentNodeState.x = 1;
+              } else {
+                currentNodeState.x = Math.random() > 0.5 ? 0 : 1;
+              }
+              currentNodeState.k = k + 1;
+
+              for (let i = 0; i < N; i++) {
+                fetch(`http://localhost:${BASE_NODE_PORT + i}/message`, {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({k: currentNodeState.k, x: currentNodeState.x, messageType: "propose"}),
+                });
+
+            }
+          }
+        }
+      }
+    }
+    return res.status(200).send("Message received and processed.");
+  });
+
+
+  node.get("/start", async (req, res) => {
+    while (!nodesAreReady()) {
+      await delay(5);
+    }
+    if (!isFaulty) {
+      currentNodeState.k = 1;
+      currentNodeState.x = initialValue;
+      currentNodeState.decided = false;
+      for (let i = 0; i < N; i++) {
+        fetch(`http://localhost:${BASE_NODE_PORT + i}/message`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ k: currentNodeState.k, x: currentNodeState.x, messageType: "propose" }),
+        });
+      }
+    }
+    else {
+      currentNodeState.decided = null;
+      currentNodeState.x = null;
+      currentNodeState.k = null;
+    }
+    return res.status(200).send("Ben or Consensus algorithm started.");
+  });
+
+  const server = node.listen(BASE_NODE_PORT + nodeId, async () => {
+    console.log(`Node ${nodeId} is listening on port ${BASE_NODE_PORT + nodeId}`);
     setNodeIsReady(nodeId);
   });
 
